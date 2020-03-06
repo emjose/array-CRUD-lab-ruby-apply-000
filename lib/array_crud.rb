e986883981314612085c5d0ec7f94d583cff1e64
Emmanuel Jose <emjose@gmail.com>
2020-03-06 22:33:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   names = ["Adam", "Bob", "Christine", "Dana"]
-  names << "Eric"
-  puts names[4]
+  names.unshift ("Zero name")
+  
 end
 
 def add_element_to_start_of_array(array, element)
